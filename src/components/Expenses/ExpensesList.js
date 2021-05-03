45fc1a02e1a17824e82da2a53d8fa34f18a97b7a
Anup Krishna Kumar <akumar11@mt.iitr.ac.in>
2021-05-03 09:56:55 +0530

    first commit

---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ExpensesList.css';
+import ExpenseItem from './ExpenseItem';
+
+const ExpensesList = (props) => {
+    let expensesContent = <p>No Expenses Found</p>;
+    if(props.expenses.length === 0){
+        return <h2 className="expenses-list__fallback">{expensesContent}</h2>
+    }
+
+    return (
+        <ul className="expenses-list">
+            {props.expenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)}
+        </ul>
+    )
+};
+
+export default ExpensesList
